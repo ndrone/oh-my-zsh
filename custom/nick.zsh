0242cd39378571d84556e3809173fb38e5aa5b66
Nicholas Drone <ndrone@gmail.com>
2019-08-06 18:22:08 -0400

    Added custom commands for zsh autojump gitignore java and volume

---
+++ custom/nick.zsh
@@ -0,0 +1,19 @@
+ZSH_THEME="lukerandall"
+
+source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
+function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
+
+# java version alias
+export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
+export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
+
+alias java8='export JAVA_HOME=$JAVA_8_HOME'
+alias java11='export JAVA_HOME=$JAVA_11_HOME'
+
+# default to Java 11
+java11
+
+function volume() { sudo osascript -e "set Volume $1";}

d829c56cd0aa7a59cbee07dca542540c1bdb89fb
Nicholas Drone <ndrone@gmail.com>
2019-11-19 11:40:53 -0500

    Created custom zsh-autosuggestions

--- custom/nick.zsh
+++ custom/nick.zsh
@@ -1,13 +1,12 @@
 ZSH_THEME="lukerandall"
 
-source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 alias zshconfig="subl ~/.zshrc"
 alias ohmyzsh="subl ~/.oh-my-zsh"
 
 # enable once https://github.com/robbyrussell/oh-my-zsh/pull/8062 is merged
-#plugins=(autojump brew extract gitignore java mac)
+#plugins=(autojump brew extract gitignore java mac zsh-autosuggestions)
 
 # remove these once the above is enabled.
 source ${ZSH}/plugins/autojump/autojump.plugin.zsh
@@ -16,3 +15,4 @@ source ${ZSH}/plugins/extract/extract.plugin.zsh
 source ${ZSH}/plugins/gitignore/gitignore.plugin.zsh
 source ${ZSH}/custom/plugins/java/java.plugin.zsh
 source ${ZSH}/custom/plugins/mac/mac.plugin.zsh
+source ${ZSH}/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh

b03f90b99b3eee8babbff502732471b053bf5769
Nicholas Drone <ndrone@gmail.com>
2019-11-19 11:43:50 -0500

    Created custom zsh-syntax-highlighting

--- custom/nick.zsh
+++ custom/nick.zsh
@@ -1,12 +1,10 @@
 ZSH_THEME="lukerandall"
 
-source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
 alias zshconfig="subl ~/.zshrc"
 alias ohmyzsh="subl ~/.oh-my-zsh"
 
 # enable once https://github.com/robbyrussell/oh-my-zsh/pull/8062 is merged
-#plugins=(autojump brew extract gitignore java mac zsh-autosuggestions)
+#plugins=(autojump brew extract gitignore java mac zsh-autosuggestions zsh-syntax-highlighting)
 
 # remove these once the above is enabled.
 source ${ZSH}/plugins/autojump/autojump.plugin.zsh
@@ -16,3 +14,4 @@ source ${ZSH}/plugins/gitignore/gitignore.plugin.zsh
 source ${ZSH}/custom/plugins/java/java.plugin.zsh
 source ${ZSH}/custom/plugins/mac/mac.plugin.zsh
 source ${ZSH}/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+source ${ZSH}/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh

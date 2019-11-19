900b2e8fd36792116dc6f57a9f124875b1b861ad
Nicholas Drone <ndrone@gmail.com>
2019-11-19 12:13:24 -0500

    Added mvn plugin

--- custom/nick.zsh
+++ custom/nick.zsh
@@ -4,13 +4,14 @@ alias zshconfig="subl ~/.zshrc"
 alias ohmyzsh="subl ~/.oh-my-zsh"
 
 # enable once https://github.com/robbyrussell/oh-my-zsh/pull/8062 is merged
-#plugins=(autojump brew extract gitignore java mac zsh-autosuggestions zsh-syntax-highlighting)
+#plugins=(autojump brew extract gitignore java mac mvn zsh-autosuggestions zsh-syntax-highlighting)
 
 # remove these once the above is enabled.
 source ${ZSH}/plugins/autojump/autojump.plugin.zsh
 source ${ZSH}/plugins/brew/brew.plugin.zsh
 source ${ZSH}/plugins/extract/extract.plugin.zsh
 source ${ZSH}/plugins/gitignore/gitignore.plugin.zsh
+source ${ZSH}/plugins/mvn/mvn.plugin.zsh
 source ${ZSH}/custom/plugins/java/java.plugin.zsh
 source ${ZSH}/custom/plugins/mac/mac.plugin.zsh
 source ${ZSH}/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh

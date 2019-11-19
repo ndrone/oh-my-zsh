1945ca773a8a6b7a35ce4edb0ef789fa1489cdd6
Nicholas Drone <ndrone@gmail.com>
2019-11-19 11:31:28 -0500

    Moved to gitignore plugin

--- custom/nick.zsh
+++ custom/nick.zsh
@@ -3,7 +3,6 @@ ZSH_THEME="lukerandall"
 source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
 function volume() { sudo osascript -e "set Volume $1";}
 function network() { networksetup -setairportpower $1 $2 ;}
 
@@ -20,9 +19,10 @@ alias java11='export JAVA_HOME=$JAVA_11_HOME'
 java11
 
 # enable once https://github.com/robbyrussell/oh-my-zsh/pull/8062 is merged
-#plugins=(autojump brew extract)
+#plugins=(autojump brew extract gitignore)
 
 # remove these once the above is enabled.
 source ${ZSH}/plugins/autojump/autojump.plugin.zsh
 source ${ZSH}/plugins/brew/brew.plugin.zsh
 source ${ZSH}/plugins/extract/extract.plugin.zsh
+source ${ZSH}/plugins/gitignore/gitignore.plugin.zsh

83b32223581729b22928768d3eb790868f90ecfb
Nicholas Drone <ndrone@gmail.com>
2019-11-20 12:19:20 -0500

    Added alias-finder plugin

--- custom/nick.zsh
+++ custom/nick.zsh
@@ -3,10 +3,13 @@ ZSH_THEME="lukerandall"
 alias zshconfig="subl ~/.zshrc"
 alias ohmyzsh="subl ~/.oh-my-zsh"
 
+ZSH_ALIAS_FINDER_AUTOMATIC=true
+
 # enable once https://github.com/robbyrussell/oh-my-zsh/pull/8062 is merged
-#plugins=(autojump brew extract git gitignore java mac mvn zsh-autosuggestions zsh-syntax-highlighting)
+#plugins=(alias-finder autojump brew extract git gitignore java mac mvn zsh-autosuggestions zsh-syntax-highlighting)
 
 # remove these once the above is enabled.
+source ${ZSH}/plugins/alias-finder/alias-finder.plugin.zsh
 source ${ZSH}/plugins/autojump/autojump.plugin.zsh
 source ${ZSH}/plugins/brew/brew.plugin.zsh
 source ${ZSH}/plugins/extract/extract.plugin.zsh

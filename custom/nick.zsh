7c42126d2ef636c355fee61535c417dcd9419dbb
Nicholas Drone <ndrone@gmail.com>
2020-03-19 11:01:07 -0400

    Move customized alias-finder to customer directory

--- custom/nick.zsh
+++ custom/nick.zsh
@@ -2,6 +2,7 @@ ZSH_THEME="af-magic2"
 
 alias zshconfig="subl ~/.zshrc"
 alias ohmyzsh="subl ~/.oh-my-zsh"
+alias af="alias-finder"
 
 ZSH_ALIAS_FINDER_AUTOMATIC=true
 
@@ -9,13 +10,13 @@ ZSH_ALIAS_FINDER_AUTOMATIC=true
 #plugins=(alias-finder autojump brew extract git gitignore java mac mvn zsh-autosuggestions zsh-syntax-highlighting)
 
 # remove these once the above is enabled.
-source ${ZSH}/plugins/alias-finder/alias-finder.plugin.zsh
 source ${ZSH}/plugins/autojump/autojump.plugin.zsh
 source ${ZSH}/plugins/brew/brew.plugin.zsh
 source ${ZSH}/plugins/extract/extract.plugin.zsh
 source ${ZSH}/plugins/git/git.plugin.zsh
 source ${ZSH}/plugins/gitignore/gitignore.plugin.zsh
 source ${ZSH}/plugins/mvn/mvn.plugin.zsh
+source ${ZSH}/custom/plugins/alias-finder/alias-finder.plugin.zsh
 source ${ZSH}/custom/plugins/java/java.plugin.zsh
 source ${ZSH}/custom/plugins/mac/mac.plugin.zsh
 source ${ZSH}/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh

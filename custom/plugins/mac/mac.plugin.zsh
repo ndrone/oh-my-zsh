7eb779bd00d766642ac54c12661c6f1d2d579db4
Nicholas Drone <ndrone@gmail.com>
2022-02-06 16:41:25 -0500

    moved screensaver alias to mac plugin

--- custom/plugins/mac/mac.plugin.zsh
+++ custom/plugins/mac/mac.plugin.zsh
@@ -1,3 +1,5 @@
+alias screensaver="/System/Library/CoreServices/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine"
+
 function volume() { sudo osascript -e "set Volume $1";}
 function network() {
   # $1 interface
